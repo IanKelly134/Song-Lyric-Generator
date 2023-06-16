f8560568ae954a227b0fd0237346e0c37ac41ddf
Ian Kelly <IanKelly134@gmail.com>
2023-06-16 11:45:53 -0600

    Update LyricGenerator.cs

--- LyricsAndProseGenerator/LyricsAndProseGenerator/LyricGenerator.cs
+++ LyricsAndProseGenerator/LyricsAndProseGenerator/LyricGenerator.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using System.Text;
 
 namespace LyricsAndProseGenerator;
 
 public class LyricGenerator
 {
-    public static string GenerateRandomLyrics(int linesToGenerate, string path)
+    public static string GenerateRandomLyrics(int linesToGenerate, string path, string endsWith = null)
     {
         const Int32 BufferSize = 128;
         using (var fileStream = File.OpenRead(path))
@@ -16,22 +16,33 @@ public class LyricGenerator
                 List<string> lines = new List<string>();
                 while ((line = streamReader.ReadLine()) != null)
                 {
-                    lines.Add(line);
+                    if (!string.IsNullOrWhiteSpace(line))
+                    {
+                        if (!string.IsNullOrWhiteSpace(endsWith))
+                        {
+                            if (line.ToLower().EndsWith(endsWith.ToLower()))
+                                lines.Add(line);
+                        }
+                        else
+                        {
+                            lines.Add(line);
+                        }
+                          
+                    }
                 }
 
                 Random rnd = new Random();
                 StringBuilder sb = new StringBuilder();
                 sb.AppendLine(" ");
                 sb.AppendLine("--------------------------------------------------------------");
+
                 for (int i = 0; i < linesToGenerate; i++)
                 {
                     int indx = rnd.Next(0, lines.Count - 1);
                     string val = lines[indx];
-                    if(!string.IsNullOrWhiteSpace(val))
-                        sb.AppendLine(val);
-                    else
-                        i--;
+                    sb.AppendLine(val);
                 }
+
                 sb.AppendLine("--------------------------------------------------------------");
                 sb.AppendLine(" ");
                 return sb.ToString();
