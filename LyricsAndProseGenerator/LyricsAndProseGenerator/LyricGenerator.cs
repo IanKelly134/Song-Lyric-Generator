3e61e7688be18ddc1ab554d21548685bf73eed47
Ian Kelly <IanKelly134@gmail.com>
2023-06-16 11:20:55 -0600

    Add files via upload

---
+++ LyricsAndProseGenerator/LyricsAndProseGenerator/LyricGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace LyricsAndProseGenerator;
+
+public class LyricGenerator
+{
+    public static string GenerateRandomLyrics(int linesToGenerate, string path)
+    {
+        const Int32 BufferSize = 128;
+        using (var fileStream = File.OpenRead(path))
+        {
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+            {
+                String line;
+                List<string> lines = new List<string>();
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+
+                Random rnd = new Random();
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine(" ");
+                sb.AppendLine("--------------------------------------------------------------");
+                for (int i = 0; i < linesToGenerate; i++)
+                {
+                    int indx = rnd.Next(0, lines.Count - 1);
+                    string val = lines[indx];
+                    if(!string.IsNullOrWhiteSpace(val))
+                        sb.AppendLine(val);
+                    else
+                        i--;
+                }
+                sb.AppendLine("--------------------------------------------------------------");
+                sb.AppendLine(" ");
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ LyricsAndProseGenerator/LyricsAndProseGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LyricsAndProseGenerator;
+
+
+string main = "Enter number of lyric lines to generate - Enter XX to exit :";
+Console.WriteLine(main);
+string line;
+while ((line = Console.ReadLine()) != "XX")
+{
+    if (!Int32.TryParse(line, out Int32 val))
+    {
+        Console.WriteLine("Invalid Entry...");
+        Console.WriteLine(main);
+        continue;
+    }
+
+    string result = LyricGenerator.GenerateRandomLyrics(Convert.ToInt32(line), @"C:\Users\IAN_KELLY\OneDrive - S&P Global\Documents\Personal\Personal\Lyrics.txt");
+    Console.WriteLine(result);
+    Console.WriteLine(main);
+}
